1c1607e60e33e5fc961fa89c88a8097d409c7d43
Wan2-sa <alvan.wani>
2025-09-05 16:50:55 +0200

    submitted week 6 assignment

---
+++ answers.sql
@@ -0,0 +1,23 @@
+-- Week 6 Assignment: Joins and Relationships
+
+USE salesDB;
+
+-- Question 1
+
+SELECT e.firstName, e.lastName, e.email, o.officeCode
+FROM employees e
+INNER JOIN offices o ON e.officeCode = o.officeCode;
+
+-- Question 2
+
+SELECT p.productName, p.productVendor, pl.productLine
+FROM products p
+LEFT JOIN productlines pl ON p.productLine = pl.productLine;
+
+-- Question 3
+
+SELECT o.orderDate, o.shippedDate, o.status, o.customerNumber
+FROM customers c
+RIGHT JOIN orders o ON c.customerNumber = o.customerNumber
+ORDER BY o.orderDate
+LIMIT 10;
